9d5cf9931c8b9847260b439d186c7f2dd90acaf5
myusername <chyharov@gmail.com>
2023-04-28 00:12:53 +0300

    first test RRD

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './Header/Header';
+// import { BrowserRouter as Routes, Router, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom'
 import Pizza from 'pages/Pizza/Pizza';
 import Cart from 'pages/Cart/Cart';
 import Home from 'pages/Home/Home';
+import Header from './Header/Header';
 
 export const App = () => {
   const [cartItems, setCartItems] = useState(() => {
@@ -33,20 +34,40 @@ export const App = () => {
   };
 
   return (
-    <Router>
-      <Header cartItemCount={getCartItemCount()} />
+    <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Pizza</Link>
+        <Link to='/cart'>Cart</Link>
+      </nav>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route
-          path="/pizza"
-          element={<Pizza addToCart={addToCart} removeFromCart={removeFromCart} cartItemCount={getCartItemCount()} />}
-        />
-        <Route
-          path="/cart"
-          element={<Cart cartItems={cartItems} addToCart={addToCart} removeFromCart={removeFromCart} cartItemCount={getCartItemCount()} />}
-        />
-        <Route path="*" element={<Navigate to="/home" />} />
+
+      <Route
+        path='/'
+        element={
+        <div>
+        <p>Its a home page</p>
+      </div>}>
+      </Route>
+
+      <Route
+        path='/pizza'
+        element={
+        <div>
+        <p>Its a pizza page</p>
+      </div>}>
+      </Route>
+
+      <Route
+        path='/cart'
+        element={
+        <div>
+        <p>Its a cart page</p>
+      </div>}>
+      </Route>
+
       </Routes>
-    </Router>
+    </>
+    
   );
 };
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from 'components/Header/Header';
+
+const SharedLayout = () => {
+    return (
+      <>
+        <Suspense>
+          <Header />
+          <Suspense>
+            <main>
+              <Outlet />
+            </main>
+          </Suspense>
+        </Suspense>
+      </>
+    );
+  };
+  
+  export default SharedLayout;
